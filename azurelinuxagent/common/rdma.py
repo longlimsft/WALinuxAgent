dd671f89ebf80ba3e2b72d58680b9bf408560fc8
Long Li <longli@microsoft.com>
2020-11-19 21:21:22 +0000

    ndversion_binary

--- azurelinuxagent/common/rdma.py
+++ azurelinuxagent/common/rdma.py
@@ -94,13 +94,17 @@ class RDMAHandler(object):
             logger.error(error_msg % driver_info_source)
             return
 
-        f = open(driver_info_source)  # pylint: disable=C0103
+        f = open(driver_info_source, "rb")  # pylint: disable=C0103
         while True:
             key = f.read(kvp_key_size)
             value = f.read(kvp_value_size)
             if key and value:
-                key_0 = key.split("\x00")[0]
-                value_0 = value.split("\x00")[0]
+                key_0 = key.partition(b"\x00")[0]
+                if key_0 :
+                    key_0 = key_0.decode()
+                value_0 = value.partition(b"\x00")[0]
+                if value_0 :
+                    value_0 = value_0.decode()
                 if key_0 == "NdDriverVersion":
                     f.close()
                     self.nd_version = value_0
